433f80ff7baee63a3e56e9fd19e9ae25025315d4
kade-shikaal <skadebis@gmail.com>
2019-11-28 20:29:45 +0200

    Updates

---
+++ precache-manifest.3a0b7054a859ad6fc78e5bdbbc7271b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8805b598cb97261e8f73705b45c8f1",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "f28a02c33a6b33ca8a37",
+    "url": "/videos/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "58668861d025c87de99d",
+    "url": "/videos/static/js/2.d87da591.chunk.js"
+  },
+  {
+    "revision": "f28a02c33a6b33ca8a37",
+    "url": "/videos/static/js/main.c22685fa.chunk.js"
+  },
+  {
+    "revision": "c1412fb9867b9092fb2d",
+    "url": "/videos/static/js/runtime-main.12b1e5ae.js"
+  }
+]);
